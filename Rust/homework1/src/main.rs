204bd6875ff9a3e6f87838e2181b6e408a252c48
HazzyWazz <valek.daniel@bottyan.eu>
2025-09-26 20:52:30 +0200

    Fix edge case i mean bug i mean edgebug EDGEBUG???

--- Rust/homework1/src/main.rs
+++ Rust/homework1/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 use std::io::{self, Write, Result}; // import std::io
-use std::cmp;
+// use std::cmp;
 
 fn prompt() -> Result<String> {
 	// ensure the above is outputted before reading input, returns the error if there's an error flushing
@@ -19,27 +19,50 @@ fn main() {
 	let y: i32 = prompt().expect("Invalid value!").parse().expect("Invalid value!");
 	let z: i32 = prompt().expect("Invalid value!").parse().expect("Invalid value!");
 
-	let x_abs = x.abs();
-	let y_abs = y.abs();
-	let z_abs = z.abs();
+	// let x_abs = x.abs();
+	// let y_abs = y.abs();
+	// let z_abs = z.abs();
 
-	let max = cmp::max(cmp::max(x_abs, y_abs), z_abs);
+	let mut max = x;
 
-	if x_abs == y_abs || z_abs == y_abs || x_abs == z_abs {
-		if max == x {
-			print!("{}", x);
-		} else if max == y {
-			print!("{}", y);
-		} else if max == z {
-			print!("{}", z);
-		}
-	} else {
-		if max == x_abs {
-			print!("{}", x);
-		} else if max == y_abs {
-			print!("{}", y);
-		} else if max == z_abs {
-			print!("{}", z);
-		}
+	if y.abs() > max.abs() {
+		max = y;
+	} else if y.abs() == max.abs() && y > max {
+		max = y;
 	}
+	if z.abs() > max.abs() {
+		max = z;
+	} else if z.abs() == max.abs() && z > max {
+		max = z;
+	}
+	println!("{}", max);
+
+	/*
+	A lenti kód hibás
+	57, -57, 1 -re 57-et ír, de
+	-57, 57, 1 -re -57-et ir
+
+	The code below fails for the condition -57, 57, 1, as it returns -57
+	*/ 
+	
+
+	// let max = cmp::max(cmp::max(x_abs, y_abs), z_abs);
+
+	// if x_abs == y_abs || z_abs == y_abs || x_abs == z_abs {
+	// 	if max == x_abs {
+	// 		print!("{}", x);
+	// 	} else if max == y_abs {
+	// 		print!("{}", y);
+	// 	} else if max == z_abs {
+	// 		print!("{}", z);
+	// 	}
+	// } else {
+	// 	if max == x_abs {
+	// 		print!("{}", x);
+	// 	} else if max == y_abs {
+	// 		print!("{}", y);
+	// 	} else if max == z_abs {
+	// 		print!("{}", z);
+	// 	}
+	// }
 }
